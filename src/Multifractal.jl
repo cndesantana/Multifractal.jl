3f7899a883a4b2dd92e8b6a6e576fc81d6bba23d
cndesantana <charles.desantana@eawag.ch>
2015-12-26 19:39:52 +0100

    Prototype of the functions

--- src/Multifractal.jl
+++ src/Multifractal.jl
@@ -7,6 +7,15 @@ using Vega
 function start()
     println("Hello multifractal world!");
 end
+
+function fitting(Array{Float64,1}::vx, Array{Float64,1}::vy, Int64::N)
+    println("Fitting function");
+end
+
+function calc_SumM(Array{Float64,1}::x,Array{Float64,1}::y,Float64::Ei,Float64::Ef,Int64::N)
+    println("calcSum function");
+end
+
 #Write the functions here
 
 end #module
