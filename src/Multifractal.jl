a98921604192bc68f64bb68b4e4342ed4b8fa293
lucasfr <lucas.fr6@gmail.com>
2015-12-27 00:31:49 +0100

    Fixing typing errors

--- src/Multifractal.jl
+++ src/Multifractal.jl
@@ -8,11 +8,11 @@ function start()
     println("Hello multifractal world!");
 end
 
-function fitting(Array{Float64,1}::vx, Array{Float64,1}::vy, Int64::N)
+function fitting(vx::Array{Float64,1}, vy::Array{Float64,1}, N::Int64)
     println("Fitting function");
 end
 
-function calc_SumM(Array{Float64,1}::x,Array{Float64,1}::y,Float64::Ei,Float64::Ef,Int64::N)
+function calc_SumM(x::Array{Float64,1},y::Array{Float64,1},Ei::Float64,Ef::Float64,N::Int64)
     println("calcSum function");
 end
 
