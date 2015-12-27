8d06668da0a9eee0858818f7eea81cff8799373a
cndesantana <charles.desantana@eawag.ch>
2015-12-27 01:12:32 +0100

    Fixing bugs

--- src/Multifractal.jl
+++ src/Multifractal.jl
@@ -4,16 +4,16 @@ module Multifractal
 
 using Vega
 
-function ChhabraJansen(ASCIIString::inputfile,ASCIIString::extensionDq,ASCIIString::extensionFa,Float64::initialQ,Float64::finalQ,Float64::dq,Int64::Np,Float64::r2dq,Float64::r2fa,Int64::typeofoutput,Int64::scalesToRemove)
+function ChhabraJansen(inputfile::ASCIIString, extensionDq::ASCIIString, extensionFa::ASCIIString, initialQ::Float64, finalQ::Float64, dq::Float64, Np::Int64, r2dq::Float64, r2fa::Float64, scalesToRemove::Int64)
     println("Chhabra-Jansen multifractal method!");
 end
 
-function fitting(Array{Float64,1}::vx, Array{Float64,1}::vy, Int64::N)
+function fitting(vx::Array{Float64,1}, vy::Array{Float64,1}, N::Int64)
     println("Fitting function");
 end
 
-function calcSumM(Array{Float64,1}::x,Array{Float64,1}::y,Float64::Ei,Float64::Ef,Int64::N)
-    println("calcSum function");
+function calcSumM(x::Array{Float64,1}, y::Array{Float64,1}, Ei::Float64, Ef::Float64, N::Int64)
+    println("CalcSum function");
 end
 
 #Write the functions here
