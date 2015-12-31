b9ca3dc32ff8b0ef1feb99bdf3f778a48e510f4e
cndesantana <charles.desantana@eawag.ch>
2015-12-31 09:46:46 +0100

    Adding the Brownian motion example

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Multifractal 
+using Benchmarks
 
 function main(inputfile::ASCIIString, extensionDq::ASCIIString, extensionFa::ASCIIString, extensionTau::ASCIIString, initialQ::Float64, finalQ::Float64, dq::Float64, Np::Int64, r2dq::Float64, r2fa::Float64, scalesToRemove::Int64)
 
@@ -13,4 +14,5 @@ end
 
 #main(ARGS[1], ARGS[2], ARGS[3], "tau", parse(Float64,ARGS[4]), parse(Float64,ARGS[5]), parse(Float64,ARGS[6]), parse(Int,ARGS[7]), parse(Float64,ARGS[8]), parse(Float64,ARGS[9]),parse(Int,ARGS[10]));
 
-main("series.txt","tdq","tfa","tau",-5.0,5.0,1.0,9,-1.0,-1.0,1);
+@benchmark main("eeg.txt","tdq","tfa","tau",-5.0,5.0,1.0,9,-1.0,-1.0,1);
+@benchmark main("brown.txt","tdq","tfa","tau",-5.0,5.0,1.0,9,-1.0,-1.0,1);
